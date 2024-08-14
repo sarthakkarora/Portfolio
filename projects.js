f06760ce731877b93c9fa5a4c1036ef24aeb0d20
Sarthak Arora <sarthakk.arora1@gmail.com>
2024-08-14 10:48:45 +0530

    projects.js

--- projects.js
+++ projects.js
@@ -1,19 +1,19 @@
-document.getElementById('toggleMode').addEventListener('click', function() {
-    document.body.classList.toggle('light-mode');
+// document.getElementById('toggleMode').addEventListener('click', function() {
+//     document.body.classList.toggle('light-mode');
     
-    // Optionally, you can use localStorage to save the user's preference
-    if (document.body.classList.contains('light-mode')) {
-      localStorage.setItem('theme', 'light');
-    } else {
-      localStorage.setItem('theme', 'dark');
-    }
-  });
+//     // Optionally, you can use localStorage to save the user's preference
+//     if (document.body.classList.contains('light-mode')) {
+//       localStorage.setItem('theme', 'light');
+//     } else {
+//       localStorage.setItem('theme', 'dark');
+//     }
+//   });
   
-  // Apply saved theme on page load
-  window.addEventListener('load', function() {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'light') {
-      document.body.classList.add('light-mode');
-    }
-  });
+//   // Apply saved theme on page load
+//   window.addEventListener('load', function() {
+//     const savedTheme = localStorage.getItem('theme');
+//     if (savedTheme === 'light') {
+//       document.body.classList.add('light-mode');
+//     }
+//   });
   
